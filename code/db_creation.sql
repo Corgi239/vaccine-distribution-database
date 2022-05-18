8282af5223ed167cd72ca2e440dc1086fc96dbc1
Corgi239 <89700445+Corgi239@users.noreply.github.com>
2022-05-18 16:34:02 +0300

    Start work on db creation sql code

---
+++ code/db_creation.sql
@@ -0,0 +1,54 @@
+CREATE TABLE VaccineData(
+    vaccineID VARCHAR(10) NOT NULL,
+    nrOfDoses INT NOT NULL CHECK (nrOfDoses = 1 OR nrOfDoses = 2),
+    criticalTemperature INT NOT NULL, 
+
+    PRIMARY KEY (vaccineID)
+);
+
+CREATE TABLE Manufacturer(
+    ID VARCHAR(10) NOT NULL,
+    origin VARCHAR(50) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    vaccineID VARCHAR(10) NOT NULL,
+
+    FOREIGN KEY (vaccineID) REFERENCES VaccineData(vaccineID),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE MedicalFacility(
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(200) NOT NULL, 
+    phone VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (name)
+);
+
+CREATE TABLE VaccinationBatch(
+    batchID VARCHAR(10) NOT NULL,
+    amount INT NOT NULL,
+    manufDate DATE NOT NULL,
+    expDate DATE NOT NULL,
+    manufID VARCHAR(10) NOT NULL,
+    vaccineID VARCHAR(10) NOT NULL,
+    initialReceiver VARCHAR(100) NOT NULL,
+
+    FOREIGN KEY (vaccineID) REFERENCES VaccineData(vaccineID),
+    FOREIGN KEY (manufID) REFERENCES Manufacturer(ID),
+    FOREIGN KEY (initialReceiver) REFERENCES MedicalFacility(name),
+    PRIMARY KEY (batchID)
+);
+
+CREATE TABLE TransportationLog(
+    ID INT NOT NULL,
+    departureDate DATE NOT NULL,
+    arrivalDate DATE NOT NULL,
+    batchID VARCHAR(10) NOT NULL,
+    senderName VARCHAR(100) NOT NULL,
+    receiverName VARCHAR(100) NOT NULL,
+
+    FOREIGN KEY (senderName) REFERENCES MedicalFacility(name),
+    FOREIGN KEY (receiverName) REFERENCES MedicalFacility(name),
+    FOREIGN KEY (batchID) REFERENCES VaccinationBatch(batchID),
+    PRIMARY KEY (ID)
+);
