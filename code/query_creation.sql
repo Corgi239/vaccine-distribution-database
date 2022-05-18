5b4e3f5910a1b06ca72d7df082a3f7f8d5586a21
Pham Hieu <hieu.pham@aalto.fi>
2022-05-18 22:20:35 +0300

    Small update

--- code/query_creation.sql
+++ code/query_creation.sql
@@ -4,7 +4,7 @@
 
 SELECT ssNo, name, phone, role, vaccinationStatus, MedicalFacility.location
 FROM StaffMember, Employed, MedicalFacility, VaccinationEvent
-WHERE StaffMember.ssNo = Employed.StaffSSNo AND Employed.location = MedicalFacility.name AND MedicalFacility.name = VaccinationEvent.location AND VaccinationEvent.date = '2021-05-10'
+WHERE StaffMember.employer = MedicalFacility.name AND MedicalFacility.name = VaccinationEvent.location AND VaccinationEvent.date = '2021-05-10'
 
 
 /* 2
