72bbc2980ad5a8942775a3eb8c9e22d61574b8ba
Pham Hieu <hieu.pham@aalto.fi>
2022-05-18 23:32:19 +0300

    update 3 and 4

--- code/query_creation.sql
+++ code/query_creation.sql
@@ -15,9 +15,31 @@ WHERE StaffMember.ssNo = WorkOn.staffSSNo AND StaffMember.role = "doctor" AND Wo
 
 /* 3
 */
-SELECT VaccinationBatch.batchID AS batchID, VaccinationBatch.initialReceiver AS intendedLocation, CurrentState.location as currentLocation
-FROM VaccinationBatch, (SELECT VaccinationBatch.batchID AS ID, MAX(TransportationLog.arrivalDate), TransportationLog.receiverName AS location
-                        FROM VaccinationBatch, TransportationLog
-                        WHERE VaccinationBatch.batchID = TransportationLog.batchID
+/* 3.1 */
+SELECT VaccinationBatch.batchID AS batchID, VaccinationBatch.initialReceiver AS intendedLocation, CurrentState.location as currentLocation, CurrentState.phoneNumber
+FROM VaccinationBatch, (SELECT VaccinationBatch.batchID AS ID, MAX(TransportationLog.arrivalDate), TransportationLog.receiverName AS location, MedicalFacility.phone AS phoneNumber
+                        FROM VaccinationBatch, TransportationLog, MedicalFacility
+                        WHERE VaccinationBatch.batchID = TransportationLog.batchID AND VaccinationBatch.initialReceiver = MedicalFacility.name
                         GROUP BY VaccinationBatch.batchID) AS CurrentState
 WHERE VaccinationBatch.batchID = CurrentState.ID, VaccinationBatch.initialReceiver != CurrentState.location
+
+/* 3.2 */
+SELECT ID, intendedLocation, currentLocation, phoneNumber
+FROM (SELECT VaccinationBatch.batchID AS ID, MAX(TransportationLog.arrivalDate), TransportationLog.receiverName AS currentLocation, VaccinationBatch.initialReceiver AS intendedLocation,MedicalFacility.phone AS phoneNumber
+      FROM VaccinationBatch, TransportationLog, MedicalFacility
+      WHERE VaccinationBatch.batchID = TransportationLog.batchID AND VaccinationBatch.initialReceiver = MedicalFacility.name
+      GROUP BY VaccinationBatch.batchID) AS CurrentState
+WHERE intendedLocation != currentLocation
+
+/* 4
+*/
+
+SELECT VaccinatedCriticalPatient.patient, VaccinationEvent.batchID, VaccinationBatch.vaccineID, VaccinationEvent.date, VaccinationEvent.location
+FROM VaccinationEvent, 
+(SELECT Patient.ssNo AS patient
+FROM Patient, Attend, Diagnosed, Symptom
+WHERE Patient.ssNo = Attend.patient AND Attend.date > '2021-05-10' AND Patient.ssNo = Diagnosed.patient AND Diagnosed.sympyom = Symptom.name) AS VaccinatedCriticalPatient, 
+VaccinationBatch, Attend
+WHERE VaccinatedCriticalPatient.patient = Attend.patient AND Attend.date = VaccinationEvent.date AND Attend.location = VaccinationEvent.location AND VaccinationEvent = VaccinationBatch
+
+
