5a24822a3980a99f646bcdfb3d289fb1863837d6
Hieu_Pham <hieu.pham@aalto.fi>
2022-05-20 15:16:43 +0300

    changes to test excel file

--- data/test_read_excel.py
+++ data/test_read_excel.py
@@ -11,3 +11,22 @@ df_patients = pd.read_excel('data/vaccine-distribution-data.xlsx', sheet_name="P
 df_vaccine_patients = pd.read_excel('data/vaccine-distribution-data.xlsx', sheet_name="VaccinePatients")
 df_symptoms = pd.read_excel('data/vaccine-distribution-data.xlsx', sheet_name="Symptoms")
 df_diagnosis = pd.read_excel('data/vaccine-distribution-data.xlsx', sheet_name="Diagnosis")
+
+from sqlalchemy import create_engine,event,schema,Table,text
+from sqlalchemy_utils import database_exists,create_database
+
+SQLITE_SRV = 'sqlite:///'
+DB_NAME_ = 'data/query.db'
+
+engine = create_engine(SQLITE_SRV + DB_NAME_, echo = False)
+db_conn = engine.connect()
+
+df_vaccine_batch.to_sql('VaccineBatch', db_conn, if_exists='replace')
+
+query = """
+        SELECT *
+        FROM VaccineBatch
+        """
+
+tx_ = pd.read_sql_query(query, db_conn)
+print(tx_)
