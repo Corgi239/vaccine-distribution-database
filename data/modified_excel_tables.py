2f38f7a83e3512006fd44e7a6fbd8fede7b2dbdd
Duong Le <dng09010802v.2@gmail.com>
2022-05-21 12:53:42 +0300

    change dataframe creation

--- data/modified_excel_tables.py
+++ data/modified_excel_tables.py
@@ -18,97 +18,75 @@ df_diagnosis = pd.read_excel('data/vaccine-distribution-data.xlsx', sheet_name="
 
 # Creating corresponding data frames:
 
-<<<<<<< HEAD
-vaccineData = df_vaccine_type
-vaccineData.columns = ['vaccineID', 'name', 'nrOfDoses', 'tempMin', 'tempMax']
-print(vaccineData)
-
-manufacturer = df_manufacturer
-manufacturer.columns = ['ID', 'origin', 'phone', 'vaccineID']
-print(manufacturer)
-
-vaccinationBatch = df_vaccine_batch
-vaccinationBatch.columns = ['batchID', 'amount', 'manufDate', 'expDate', 'manufID', 'vaccineID', 'initialReceiver']
-print(vaccinationBatch)
-=======
 
 ## VaccineData
-vaccine_data = df_vaccine_type
+vaccine_data = df_vaccine_type[[col for col in df_vaccine_type if not col.startswith('Unnamed:')]]
 vaccine_data.columns = ['vaccineID', 'name', 'nrOfDoses', 'tempMin', 'tempMax']
 print('VaccineData: ')
 print(vaccine_data.head())
 
 ## Manufacturer
-manufacturer = df_manufacturer
+manufacturer = df_manufacturer[[col for col in df_manufacturer if not col.startswith('Unnamed:')]]
 manufacturer.columns = ['ID', 'origin', 'phone', 'vaccineID']
 print('Manufacturer: ')
 print(manufacturer.head())
 
 ## Vaccinationbatch
-vaccinationBatch = df_vaccine_batch
-vaccinationBatch.columns = ['batchID', 'amount', 'manufDate', 'expDate', 'manufID', 'vaccineID', 'initialReceiver']
+vaccinationBatch = df_vaccine_batch[[col for col in df_vaccine_batch if not col.startswith('Unnamed:')]]
+vaccinationBatch.columns = ['batchID', 'amount', 'vaccineID', 'manufID', 'manufDate', 'expDate', 'initialReceiver']
 print('VaccinationBatch: ')
 print(vaccinationBatch.head())
->>>>>>> a233064d5ad5bea1f455c7dfd5af851b147c1c93
 
 ## MedicalFacility
-medicalFacility = df_vaccination_stations
+medicalFacility = df_vaccination_stations[[col for col in df_vaccination_stations if not col.startswith('Unnamed:')]]
 medicalFacility.columns = ['name', 'address', 'phone']
-<<<<<<< HEAD
-print(medicalFacility)
-
-transportationLog = df_transportation_log
-transportationLog.columns = ['batchID', 'receiverName', 'senderName',  'arrivalDate', 'departureDate']
-transportationLog['ID'] = transportationLog.index
-print(transportationLog)
-
-staffMembers = df_staff_members
-staffMembers.columns = ['ssNo', 'name', 'phone', 'birthday', 'vaccinationStatus', 'role', 'employer']
-=======
 print('MedicalFacility:')
 print(medicalFacility)
 
 ## TransportationLog
-transportationLog = df_transportation_log
+transportationLog = df_transportation_log[[col for col in df_transportation_log if not col.startswith('Unnamed:')]]
 transportationLog.columns = ['batchID', 'receiverName', 'senderName',  'arrivalDate', 'departureDate']
 transportationLog['ID'] = transportationLog.index
 print('TransportationLog: ')
 print(transportationLog.head())
->>>>>>> a233064d5ad5bea1f455c7dfd5af851b147c1c93
 
+## StaffMembers
+staffMember = df_staff_members[[col for col in df_staff_members if not col.startswith('Unnamed:')]]
+staffMember.columns = ['ssNo', 'name', 'birthday', 'phone', 'role', 'vaccinationStatus', 'employer']
+print('StaffMember: ')
+print(staffMember.head())
 
 ## VaccinationShift
-vaccination_shifts = df_shifts[['weekday']]
+vaccination_shifts = df_shifts[[col for col in df_shifts if not col.startswith('Unnamed:')]]
 print('VaccinationShitfs: ')
 print(vaccination_shifts.head())
 
 ## VaccinationEvent
-vaccination_event = df_vaccinations
+vaccination_event = df_vaccinations[[col for col in df_vaccinations if not col.startswith('Unnamed:')]]
 vaccination_event['weekday'] = pd.Series(vaccination_event['date']).dt.day_name()
-<<<<<<< HEAD
-#print(vaccination_event)
-=======
->>>>>>> a233064d5ad5bea1f455c7dfd5af851b147c1c93
 print('VaccinationEvent: ')
 print(vaccination_event.head())
 
 ## Patient 
-patient = df_patients.rename(columns={'date of birth': 'birthday'})
+patient = df_patients[[col for col in df_patients if not col.startswith('Unnamed:')]]
+patient = patient.rename(columns={'date of birth': 'birthday'})
 print('Patient: ')
 print(patient.head())
 
 ## Attend
-attend = df_vaccine_patients.rename(columns={'patientSsNo': 'patient'})
+attend = df_vaccine_patients[[col for col in df_vaccine_patients if not col.startswith('Unnamed:')]]
+attend = attend.rename(columns={'patientSsNo': 'patient'})
 print('Attend: ')
 print(attend.head())
 
 ## Symptom
-symptom = df_symptoms.rename(columns = {'criticality':'critical'})
+symptom = df_symptoms[[col for col in df_symptoms if not col.startswith('Unnamed:')]]
+symptom =   symptom.rename(columns = {'criticality':'critical'})
 print('Symptom:')
 print(symptom.head())
 
 ## Diagnosed
 
-diagnosed = df_diagnosis
+diagnosed = df_diagnosis[[col for col in df_diagnosis if not col.startswith('Unnamed:')]]
 print('Diagnosed')
 print(diagnosed.head())
