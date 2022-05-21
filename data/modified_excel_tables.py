35752564cd91f0dbe93752f018d0fc72b29369ac
Hieu_Pham <hieu.pham@aalto.fi>
2022-05-21 14:04:34 +0300

    Exporting dataframes to csv files

--- data/modified_excel_tables.py
+++ data/modified_excel_tables.py
@@ -22,12 +22,14 @@ vaccine_data = df_vaccine_type[[col for col in df_vaccine_type if not col.starts
 vaccine_data.columns = ['vaccineID', 'name', 'nrOfDoses', 'tempMin', 'tempMax']
 print('VaccineData: ')
 print(vaccine_data.head())
+vaccine_data.to_csv('data/VaccineData.csv', index = False)
 
 ## Manufacturer
 manufacturer = df_manufacturer[[col for col in df_manufacturer if not col.startswith('Unnamed:')]]
 manufacturer.columns = ['ID', 'origin', 'phone', 'vaccineID']
 print('Manufacturer: ')
 print(manufacturer.head())
+manufacturer.to_csv('data/Manufacturer.csv', index = False)
 
 ## Vaccinationbatch
 vaccinationBatch = df_vaccine_batch[[col for col in df_vaccine_batch if not col.startswith('Unnamed:')]]
@@ -38,12 +40,14 @@ vaccinationBatch['expDate'] = pd.to_datetime(vaccinationBatch['expDate'])
 vaccinationBatch = vaccinationBatch.dropna(axis = 0, subset=['manufDate', 'expDate'])
 print('VaccinationBatch: ')
 print(vaccinationBatch.head())
+vaccinationBatch.to_csv('data/VaccinationBatch.csv', index=False)
 
 ## MedicalFacility
 medicalFacility = df_vaccination_stations[[col for col in df_vaccination_stations if not col.startswith('Unnamed:')]]
 medicalFacility.columns = ['name', 'address', 'phone']
 print('MedicalFacility:')
 print(medicalFacility)
+medicalFacility.to_csv('data/MedicalFacility.csv', index = False)
 
 ## TransportationLog
 transportationLog = df_transportation_log[[col for col in df_transportation_log if not col.startswith('Unnamed:')]]
@@ -55,6 +59,7 @@ transportationLog['arrivalDate'] = pd.to_datetime(transportationLog['arrivalDate
 transportationLog = transportationLog.dropna(axis=0, subset=['departureDate', 'arrivalDate'])
 print('TransportationLog: ')
 print(transportationLog.head())
+transportationLog.to_csv('TransportationLog.csv', index = False)
 
 ## StaffMembers
 staffMember = df_staff_members[[col for col in df_staff_members if not col.startswith('Unnamed:')]]
@@ -62,11 +67,13 @@ staffMember.columns = ['ssNo', 'name', 'birthday', 'phone', 'role', 'vaccination
 staffMember = staffMember.reindex(columns = ['ssNo', 'name', 'phone', 'birthday', 'vaccinationStatus', 'role', 'employer'])
 print('StaffMember: ')
 print(staffMember.head())
+staffMember.to_csv('data/StaffMember.csv', index = False)
 
 ## VaccinationShift
 vaccination_shifts = df_shifts[[col for col in df_shifts if not col.startswith('Unnamed:')]]
 print('VaccinationShitfs: ')
 print(vaccination_shifts.head())
+vaccination_shifts.to_csv('data/VaccinationShift.csv', index = False)
 
 ## VaccinationEvent
 vaccination_event = df_vaccinations[[col for col in df_vaccinations if not col.startswith('Unnamed:')]]
@@ -75,12 +82,14 @@ vaccination_event = vaccination_event.dropna(axis=0, subset=['date'])
 vaccination_event['weekday'] = pd.Series(vaccination_event['date']).dt.day_name()
 print('VaccinationEvent: ')
 print(vaccination_event.head())
+vaccination_event.to_csv('data/VaccinationEvent.csv', index=False)
 
 ## Patient 
 patient = df_patients[[col for col in df_patients if not col.startswith('Unnamed:')]]
 patient = patient.rename(columns={'date of birth': 'birthday'})
 print('Patient: ')
 print(patient.head())
+patient.to_csv('data/Patient.csv', index = False)
 
 ## Attend
 attend = df_vaccine_patients[[col for col in df_vaccine_patients if not col.startswith('Unnamed:')]]
@@ -89,12 +98,14 @@ attend['date'] = pd.to_datetime(attend['date'], errors='coerce')
 attend = attend.dropna(axis=0, subset=['date'])
 print('Attend: ')
 print(attend.head())
+attend.to_csv('data/Attend.csv', index = False)
 
 ## Symptom
 symptom = df_symptoms[[col for col in df_symptoms if not col.startswith('Unnamed:')]]
 symptom = symptom.rename(columns = {'criticality':'critical'})
 print('Symptom:')
 print(symptom.head())
+symptom.to_csv('data/Symptom.csv', index = False)
 
 ## Diagnosed
 
@@ -103,3 +114,4 @@ diagnosed['date'] = pd.to_datetime(diagnosed['date'], errors='coerce')
 diagnosed = diagnosed.dropna(axis=0, subset=['date'])
 print('Diagnosed')
 print(diagnosed)
+diagnosed.to_csv('data/Diagnosed.csv', index = False)
